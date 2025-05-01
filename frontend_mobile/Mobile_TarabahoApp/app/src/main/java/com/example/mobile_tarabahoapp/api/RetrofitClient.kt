a545c0341ec62d1202841f80097464899927e236
Ikanstie <uniskowt@gmail.com>
2025-05-02 01:15:16 +0800

    Mobile Deployment
    Change the baseurl from local to the deployed

--- frontend_mobile/Mobile_TarabahoApp/app/src/main/java/com/example/mobile_tarabahoapp/api/RetrofitClient.kt
+++ frontend_mobile/Mobile_TarabahoApp/app/src/main/java/com/example/mobile_tarabahoapp/api/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "https://tarabaho-backend.onrender.com"
 
     // ✅ Use the custom AuthInterceptor class
     private val okHttpClient = OkHttpClient.Builder()
